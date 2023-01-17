de90973944d656fd3e64df51f75b0db23bd090f4
Josivaldo Batista <josivaldo-fb@outlook.com>
2023-01-17 00:02:54 -0300

    Record interceptor

--- str-consumer/src/main/java/com/jfb/strconsumer/configs/StringConsumerConfig.java
+++ str-consumer/src/main/java/com/jfb/strconsumer/configs/StringConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.jfb.strconsumer.configs;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -9,9 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.RecordInterceptor;
 
 import java.util.HashMap;
 
+@Log4j2
 @Configuration
 @RequiredArgsConstructor
 public class StringConsumerConfig {
@@ -34,6 +37,27 @@ public class StringConsumerConfig {
     var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
     factory.setConsumerFactory(consumerFactory);
     return factory;
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, String> validaMessageContainerFactory(
+      ConsumerFactory<String, String> consumerFactory
+  ) {
+    var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+    factory.setConsumerFactory(consumerFactory);
+    factory.setRecordInterceptor(validaMessage());
+    return factory;
+  }
+
+  private RecordInterceptor<String, String> validaMessage() {
+
+    return record -> {
+      if (record.value().contains("TEST")) {
+        log.info("Possui a palavra TESTE");
+        return record;
+      }
+      return record;
+    };
   }
 
 }
