ef8422717abf15fc634c7fd640c4d46984077c1d
Josivaldo Batista <josivaldo-fb@outlook.com>
2023-01-19 14:58:18 -0300

    up stack

--- json-consumer/src/main/java/com/jfb/jsonconsumer/models/Payment.java
+++ json-consumer/src/main/java/com/jfb/jsonconsumer/models/Payment.java
@@ -1,7 +1,5 @@
 package com.jfb.jsonconsumer.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,10 +7,8 @@ import java.io.Serializable;
 
 @ToString
 @Getter
-@Entity
 public class Payment implements Serializable {
 
-  @Id
   private Long id;
   private Long idUser;
   private Long idProduct;
